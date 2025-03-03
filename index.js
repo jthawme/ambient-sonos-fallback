51e102912828199a92692ac872db7c3cd538d405
Jonny Thaw <hi@jthaw.me>
2025-03-03 18:29:08 +0000

    fix: handled error

--- index.js
+++ index.js
@@ -18,62 +18,73 @@ const DEFAULT_OPTIONS = {};
 /**
  * @param {import("../server/types/options").PluginItemInject} inject
  */
-export const handler = async ({ events, config, spotify, sdk, history }) => {
+export const handler = async ({
+  events,
+  config,
+  spotify,
+  sdk,
+  history,
+  comms,
+}) => {
   const options = deepmerge(DEFAULT_OPTIONS, config.pluginOptions?.sonos ?? {});
 
-  const discovery = new AsyncDeviceDiscovery();
-  const devices = await discovery.discoverMultiple();
-  const devicesInfo = await Promise.all(
-    devices.map(async (device) => {
-      return device.deviceDescription();
-    })
-  );
-  const deviceObject = devicesInfo.reduce((dict, curr, idx) => {
-    return {
-      ...dict,
-      [curr.roomName]: devices[idx],
+  try {
+    const discovery = new AsyncDeviceDiscovery();
+    const devices = await discovery.discoverMultiple();
+    const devicesInfo = await Promise.all(
+      devices.map(async (device) => {
+        return device.deviceDescription();
+      })
+    );
+    const deviceObject = devicesInfo.reduce((dict, curr, idx) => {
+      return {
+        ...dict,
+        [curr.roomName]: devices[idx],
+      };
+    }, {});
+
+    /**
+     *
+     * @param {import('$server/history').CommandHistoryItem} command
+     * @returns
+     */
+    const controlSonos = async (command) => {
+      const state = await spotify.device.get(sdk.current);
+
+      const device = deviceObject[state.device.name];
+
+      if (device) {
+        switch (command.type) {
+          case "/skipBackward":
+            return device.previous();
+          case "/skipForward":
+            return device.next();
+          case "/play":
+            return device.play();
+          case "/pause":
+            return device.pause();
+          case "/add":
+            // to do
+
+            console.log(command);
+            // return device.next();
+            return;
+        }
+      }
+
+      // if (device) {
+      // 	await device.next();
+      // }
     };
-  }, {});
 
-  /**
-   *
-   * @param {import('$server/history').CommandHistoryItem} command
-   * @returns
-   */
-  const controlSonos = async (command) => {
-    const state = await spotify.device.get(sdk.current);
-
-    const device = deviceObject[state.device.name];
-
-    if (device) {
-      switch (command.type) {
-        case "/skipBackward":
-          return device.previous();
-        case "/skipForward":
-          return device.next();
-        case "/play":
-          return device.play();
-        case "/pause":
-          return device.pause();
-        case "/add":
-          // to do
-
-          console.log(command);
-          // return device.next();
-          return;
+    events.on("app:error", (evt) => {
+      if (evt?.message === "spotify/restricted") {
+        if (history.last) {
+          controlSonos(history.last);
+        }
       }
-    }
-
-    // if (device) {
-    // 	await device.next();
-    // }
-  };
-
-  events.on("app:error", (evt) => {
-    if (evt?.message === "spotify/restricted") {
-      if (history.last) {
-        controlSonos(history.last);
-      }
-    }
-  });
+    });
+  } catch (e) {
+    req.comms.message("Sonos:" + e.message ?? "error");
+  }
 };
